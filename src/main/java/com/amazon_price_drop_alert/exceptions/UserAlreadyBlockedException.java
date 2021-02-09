8809a93a4ff3ac9ca877d128b34b50d56dfac649
justynaWoj <j.wojciechowska2011@gmail.com>
2021-02-09 21:19:52 +0100

    Deleting unnecessary classes

--- src/main/java/com/amazon_price_drop_alert/exceptions/UserAlreadyBlockedException.java
+++
@@ -1,7 +0,0 @@
-package com.amazon_price_drop_alert.exceptions;
-
-public class UserAlreadyBlockedException extends Exception {
-    public UserAlreadyBlockedException(String message) {
-        super(message);
-    }
-}
