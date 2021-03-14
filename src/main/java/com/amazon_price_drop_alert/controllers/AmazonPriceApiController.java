867bfb00fc67b81bfe2ca0d133846a31754c937c
justynaWoj <j.wojciechowska2011@gmail.com>
2021-03-14 21:36:40 +0100

    Naming convention fix

--- src/main/java/com/amazon_price_drop_alert/controllers/AmazonPriceApiController.java
+++ src/main/java/com/amazon_price_drop_alert/controllers/AmazonPriceApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/v1/price")
+@RequestMapping(value = "/v1/prices")
 @AllArgsConstructor
 public class AmazonPriceApiController {
 
